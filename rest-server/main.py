3ca71c27c89f02e3acd8d7c789ab82a2c1145501
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-04-09 11:41:21 -0400

    rest and metrics communicate locally, pushing to test on GCP

--- rest-server/main.py
+++ rest-server/main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from flask import Flask
 
@@ -6,17 +7,10 @@ app = Flask(__name__)
 
 #this is a dummy function to get the metrics that would be returned from part 1 code
 #in practice, this would call the part 1 code on the package provided
-def getMetrics(package_name):
-    test_metrics = {
-        package_name : {"URL": package_name, 
-                        "NET_SCORE": 1.0, 
-                        "RAMP_UP_SCORE": 1.0, 
-                        "CORRECTNESS_SCORE": 1.0, 
-                        "BUS_FACTOR_SCORE": 1.0, 
-                        "RESPONSIVE_MAINTAINER_SCORE": 1.0, 
-                        "LICENSE_SCORE": 1.0},
-        }
-    return test_metrics
+def getMetrics(package_url):
+    request_url = "https://pt1-server-h5si5ezrea-uc.a.run.app" + package_url
+    test_metrics = requests.get(request_url) #this is HTTP response
+    return test_metrics.content.decode() #decode the content (metrics) 
 
 #this is a dummy function that would query the database for all the package names we have
 #in practice, this will connect to the database and get a list of all package names in the database
@@ -27,15 +21,18 @@ def queryAllPackages():
 #this is a dummy function that would get all information about the package from the database and display it
 #in practice, this will connect to the database and get all info about a certain package
 def getPackageInfo(package_name):
-    test_info = {
+    package_info = {
         package_name : {"name": package_name, 
+                        "url": "http://url_for_package.com",
                         "users": 1000, 
                         "downloads": 69, 
                         "stars": 100, 
                         "description": "This package does x, y, and z for you",
                         },
         }
-    return test_info
+    return package_info
+
+#-------------------------------------ENDPOINTS---------------------------------------
 
 #this is the home url of the server
 @app.route("/")
@@ -54,15 +51,15 @@ def list_packages():
 @app.route("/packages/<package_name>")
 def package_info(package_name):
     info = getPackageInfo(package_name)
-    return "Here is the information we have on the package you wanted:\n {}!".format(info)
+    return "Here is the information we have on the package you wanted:\n {}".format(info)
 
 #this will be the metrics page for a certain object
 #it will run the part 1 code on the package
 @app.route("/packages/<package_name>/metrics")
 def get_metrics(package_name):
-    package_metrics = getMetrics(package_name)
+    package_url = "https://github.com/lodash/lodash" #in production, this will get URL from database
+    package_metrics = getMetrics(package_url) 
     return "Here are the metrics you asked for:\n {}!".format(package_metrics)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
