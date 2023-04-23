ba14869930143c663fa4c3bf3bec96e8a2e5b994
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-04-23 17:51:21 -0400

    oops

--- rest-server/database/databaseFunctions.py
+++ rest-server/database/databaseFunctions.py
@@ -10,8 +10,7 @@ def getconn():
             "ece-461-project-2-database:us-central1:ece-461-main-database", # Cloud SQL Instance Connection Name
             "pymysql",
             user="root",
-            #password=os.environ['DBPW'],
-            password="461isSUPERcool!",
+            password=os.environ['DBPW'],
             db="packages_database",
             timeout=60,
             ip_type=IPTypes.PUBLIC # public IP
