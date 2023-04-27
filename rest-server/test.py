589d288a7880931ae0d9101cffbc83bdbcc722ef
zmid26 <zmid26@gmail.com>
2023-04-27 16:00:10 -0400

    test streamlined and handled
    test now handles errors properly, is labeled correctly, and prints detailed information on responses

--- rest-server/test.py
+++ rest-server/test.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import base64
+import traceback
 
 def main():
-
+    failed_tests = []
     #example_body = '{"metadata": {"Name": "string","Version": "1.2.3","ID": "string"},"data": { "Content": "string","URL": "string","JSProgram": "string"}}'
     #example_body = '{ "Version": "Exact (1.2.3)\nBounded range (1.2.3-2.1.0)\nCarat (^1.2.3)\nTilde (~1.2.0)", "Name": "string" }'
     #example_json = json.dumps(example_body)
@@ -41,10 +42,14 @@ def main():
                 "password": "correcthorsebatterystaple123(!__+@**(A'\"`;DROP TABLE packages;"
             }
         }
-        put_authenticate = requests.put('http://rest-server-h5si5ezrea-uc.a.run.app/authenticate', json=put_authenticate_req, verify=False)
+        put_authenticate = requests.put('http://rest-server-h5si5ezrea-uc.a.run.app/authenticate', json=put_authenticate_req)
+        print('\nPUT_AUTHENTICATE RESPONSE CODE: ',str(put_authenticate.content),'\n')
         count += 1
     except:
-        print("put /authenticate failed with code: " + str(put_authenticate.content))
+        failed_tests.append('PUT AUTHENTICATE')
+        traceback.print_exc()
+        print('put /authenticate failed with code above')
+
 
     #post /package - URL
     try:
@@ -52,11 +57,13 @@ def main():
             "URL": 'https://github.com/cloudinary/cloudinary_npm',
             "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"
         }
-        post_pkg_url_resp = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/package', json=post_pkg_url, verify=False)
-        print("post /package URL succeed and got response: " + str(post_pkg_url_resp.content))
+        post_pkg_url_resp = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/package', json=post_pkg_url)
+        print('\nPOST PKG 1 RESPONSE CODE: ',str(post_pkg_url_resp.content),'\n')
         count += 1
     except:
-        print("post /package URL failed with code: " + str(post_pkg_url_resp.content))
+        failed_tests.append('POST PKG URL 1')
+        traceback.print_exc()
+        print("post /package URL 1 failed with code above")
 
     #put /package/id-- come back to this one
     try:    
@@ -67,98 +74,132 @@ def main():
                 "ID": "tbd"
             },
             "data": {
-                "Content": encoded_cloudinary,
+                "Content": str(encoded_cloudinary),
                 "URL": "https://github.com/cloudinary/cloudinary_npm",
                 "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"
             }
         }
-        put_package_id_resp = requests.put('http://rest-server-h5si5ezrea-uc.a.run.app/package/id', json=put_package_id, verify=False)
+        put_package_id_resp = requests.put('http://rest-server-h5si5ezrea-uc.a.run.app/package/id', json=put_package_id)
+        print('\nPUT PKG ID RESPONSE CODE: ',str(put_package_id_resp.content),'\n')
         
         count += 1
     except:
-        print("put /package/id failed with code: " + str(put_package_id_resp.content))
+        failed_tests.append('PUT PKG ID')
+        traceback.print_exc()
+        print("put /package/id failed with code above")
 
     #get /package/id
     try:
-        get_package_id_resp = requests.get('http://rest-server-h5si5ezrea-uc.a.run.app/package/tbd', verify=False)
-        print("put /package/id succeeded with code: " + str(get_package_id_resp.content))
+        get_package_id_resp = requests.get('http://rest-server-h5si5ezrea-uc.a.run.app/package/tbd')
+        print('\nGET PKG ID RESPONSE CODE: ',str(get_package_id_resp.content),'\n')
+
         count += 1
     except:
-        print("put /package/id failed with code: " + str(get_package_id_resp.content))
+        failed_tests.append('GET PKG ID')
+        traceback.print_exc()
+        print("get pkg/id failed with code above")
 
     #get /package/id/rate
     try:
-        get_package_id_rate_resp = requests.get('http://rest-server-h5si5ezrea-uc.a.run.app/package/tbd/rate', verify=False)
-        print("put /package/id/rate succeeded with code: " + str(get_package_id_rate_resp.content))
+        get_package_id_rate_resp = requests.get('http://rest-server-h5si5ezrea-uc.a.run.app/package/tbd/rate')
+        print('\nGET PKG ID RATE RESPONSE CODE: ',str(get_package_id_rate_resp.content),'\n')
+
         count += 1
     except:
-        print("put /package/id/rate failed with code: " + str(get_package_id_resp.content))
+        failed_tests.append('GET PKG ID RATE')
+        traceback.print_exc()
+        print("get /package/id/rate failed with code above")
 
     #post /package - base64 SECOND
     try:
         post_pkg_base642 = {
-            "Content": encoded_axios,
+            "Content": str(encoded_axios),
             "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"
         }
-        post_pkg_base64_resp2 = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/package', json=post_pkg_base642, verify=False)
-        print("post /package base64 2 succeed and got response: " + str(post_pkg_base64_resp2.content))
+        post_pkg_base64_resp2 = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/package', json=post_pkg_base642)
+        print('\nPOST PKG B64 AXIOS RESPONSE CODE: ',str(post_pkg_base64_resp2.content),'\n')
+
         count += 1
     except:
-        print("post /package base64 2 failed with code: " + str(post_pkg_base64_resp2.content))
+        failed_tests.append('POST PKG BASE64 AXIOS')
+        traceback.print_exc()
+        print("post /package base64 axios failed with code above")
 
     #post /packages
     try:
         post_pkgs = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/packages', json={ "Version": "Exact (1.2.3)\nBounded range (1.2.3-2.1.0)\nCarat (^1.2.3)\nTilde (~1.2.0)", "Name": "cloudinary" })
-        print('post /packages succeeded with status', str(post_pkgs.content))
+        print('\nPOST PKGS RESPONSE CODE: ',str(post_pkgs.content),'\n')
+
         count += 1
 
     except:
-        print('post /packages failed with status', str(post_pkgs.content))
+        failed_tests.append('POST PKGS')
+        traceback.print_exc()
+        print('post /packages failed with status above')
 
     #get /package/byname/name
     try:
-        get_package_name_resp = requests.get('http://rest-server-h5si5ezrea-uc.a.run.app/package/byName/axios', verify=False)
-        print("get /package/byName succeeded with code: " + str(get_package_name_resp.content))
+        get_package_name_resp = requests.get('http://rest-server-h5si5ezrea-uc.a.run.app/package/byName/axios')
+        print('\nGET PKG BYNAME RESPONSE CODE: ',str(get_package_name_resp.content),'\n')
+
         count += 1
     except:
-        print("put /package/byName failed with code: " + str(get_package_name_resp.content))
+        failed_tests.append('GET PKG BYNAME')
+        traceback.print_exc()
+        print("get /package/byName failed with code above")
     
     #delete /package/byname/name
     try:
-        delete_package_name_resp = requests.delete('http://rest-server-h5si5ezrea-uc.a.run.app/package/byName/cloudinary', verify=False)
-        print("delete /package/byName succeeded with code: " + str(delete_package_name_resp.content))
+        delete_package_name_resp = requests.delete('http://rest-server-h5si5ezrea-uc.a.run.app/package/byName/cloudinary')
+        print('\nDEL PKG BYNAME RESPONSE CODE: ',str(delete_package_name_resp.content),'\n')
+
         count += 1
     except:
-        print("delete /package/byName failed with code: " + str(delete_package_name_resp.content))
+        failed_tests.append('DELETE PKG BYNAME')
+        traceback.print_exc()
+        print("delete /package/byName failed with code above")
 
     #delete /package/id
     try:
-        delete_package_id_resp = requests.delete('http://rest-server-h5si5ezrea-uc.a.run.app/package/tbd', verify=False)
-        print("delete /package/id succeeded with code: " + str(delete_package_id_resp.content))
+        delete_package_id_resp = requests.delete('http://rest-server-h5si5ezrea-uc.a.run.app/package/tbd')
+        print('\nDEL PKG BY ID RESPONSE CODE: ',str(delete_package_id_resp.content),'\n')
+
         count += 1
     except:
-        print("delete /package/id failed with code: " + str(delete_package_id_resp.content))
+        failed_tests.append('DEL PKG ID')
+        traceback.print_exc()
+        print("delete /package/id failed with code above")
 
     #post /package - URL
     try:
-        post_pkg_url3 = {
+        post_pkg_url2 = {
             "URL": 'https://github.com/moleculerjs/moleculer',
             "JSProgram": "if (process.argv.length === 7) {\nconsole.log('Success')\nprocess.exit(0)\n} else {\nconsole.log('Failed')\nprocess.exit(1)\n}\n"
         }
-        post_pkg_url_resp3 = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/package', json=post_pkg_url3, verify=False)
-        print("post /package URL 3 succeed and got response: " + str(post_pkg_url_resp3.content))
+        post_pkg_url_resp2 = requests.post('http://rest-server-h5si5ezrea-uc.a.run.app/package', json=post_pkg_url2)
+        print('\nPOST PKG URL2 RESPONSE CODE: ',str(post_pkg_url_resp2.content),'\n')
+
         count += 1
     except:
-        print("post /package URL 3 failed with code: " + str(post_pkg_url_resp3.content))
+        failed_tests.append('POST PKG URL 2')
+        traceback.print_exc()
+        print("post /package URL 2 failed with code above")
     
     #delete /reset
     try:
-        delete_reset_resp = requests.delete('http://rest-server-h5si5ezrea-uc.a.run.app/reset', verify=False)
-        print("delete /reset succeeded with code: " + str(delete_reset_resp.content))
+        delete_reset_resp = requests.delete('http://rest-server-h5si5ezrea-uc.a.run.app/reset')
+        print('\nDELETE RESET RESPONSE CODE: ',str(delete_reset_resp.content),'\n')
+
         count += 1
     except:
-        print("delete /reset failed with code: " + str(delete_reset_resp.content))
+        failed_tests.append('DELETE RESET')
+        traceback.print_exc()
+        print("delete /reset failed with code above")
 
-    print("passed: " + str(count) + " | failed: " + str(13 - count))
+    print("passed: " + str(count) + " | failed: " + str(12 - count))
+    print(f'score: {str(count)} / 12 -> {str(round(100*count/12,2))} %')
+    print('Failed tests:')
+    for f in failed_tests:
+        print(f)
 
 main()
